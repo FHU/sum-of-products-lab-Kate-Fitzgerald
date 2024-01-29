cf4fb6e3c9559a63d4833605cc073f2e4dd4e627
Kate-Fitzgerald <kate.fitzgerald@students.fhu.edu>
2024-01-29 13:39:43 -0600

    update main.py
    added split function and did math part

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
-def sum_of_products(list1, list2):
-    pass
+prods = []
+def sum_of_products(list1,list2):
+    for i in range(len(list1)):
+        prods.append(int(list1[i]) * int(list2[i]))
+        
+    total = sum(prods)
+    return total
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    list1 = input().split()
+    list2 = input().split()
+    print(sum_of_products(list1, list2))
